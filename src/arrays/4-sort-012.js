fed3154a370be8af202ed01f6a3a1a7d10475b75
rupeshtiwari <roopkt@gmail.com>
2021-07-14 13:15:37 -0400

    chore: sort array

--- src/arrays/4-sort-012.js
+++ src/arrays/4-sort-012.js
@@ -1,12 +1,48 @@
 // O(N) time |  O(1)
-function sort012(arr)
-{
-  
+function sort012(arr) {
+  let counter = {
+    0: 0,
+    1: 0,
+    2: 0,
+  };
+
+  for (let num of arr) {
+    if (num === 0) {
+      counter[0]++;
+    } else if (num === 1) {
+      counter[1]++;
+    } else if (num === 2) {
+      counter[2]++;
+    }
+  }
+
+  let arrayIdx = 0;
+
+  while (arrayIdx < arr.length) {
+    for (let idx = 0; idx < counter[0]; idx++) {
+      arr[arrayIdx] = 0;
+      arrayIdx++;
+    }
+
+    for (let idx = 0; idx < counter[1]; idx++) {
+      arr[arrayIdx] = 1;
+      arrayIdx++;
+    }
+
+    for (let idx = 0; idx < counter[2]; idx++) {
+      arr[arrayIdx] = 2;
+      arrayIdx++;
+    }
+  }
+
+  return arr;
 }
 
-xdescribe('kthSmallestNumber', () => {
+describe('sort 0 1 2', () => {
   it('test #1', () => {
-    expect(sort012([0 ,2 ,1 ,2 ,0])).toEqual([0 ,0, 1, 2,  2]);
+    expect(sort012([0, 2, 1, 2, 0])).toEqual([0, 0, 1, 2, 2]);
+  });
+  it('test #2', () => {
+    expect(sort012([0, 1, 0])).toEqual([0, 0, 1]);
   });
 });
-
