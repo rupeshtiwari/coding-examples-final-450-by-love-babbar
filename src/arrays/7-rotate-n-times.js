12c67e7ef1776212f52ae2800750d3db2848e28c
rupeshtiwari <roopkt@gmail.com>
2021-07-19 17:59:34 -0400

    chore: rotate

--- src/arrays/7-rotate-n-times.js
+++ src/arrays/7-rotate-n-times.js
@@ -3,35 +3,54 @@
  */
 function rotateClockWise(arr, n) {
   //code here
-  if (arr.length < 3) return;
 
   const length = arr.length;
+  if (n % length === 0) return arr;
+  if (n > length) n = n % length;
 
-  for (let idx = 0; idx < n; idx++) {
-    let temp = arr[1];
+  if (length < 2) return arr;
+
+  if (length < 3) {
+    let temp = arr[0];
     arr[0] = arr[1];
+    arr[1] = temp;
 
-    for (let idx = 2; idx < length - 1; ) {
+    return arr;
+  }
+
+  for (let i = 0; i < n; i++) {
+    let temp = arr[1];
+    arr[1] = arr[0];
+
+    for (let idx = 2; idx < length; idx++) {
       let current = arr[idx];
       arr[idx] = temp;
-      temp = arr[idx];
+      temp = current;
     }
 
-    arr[0] = arr[length - 1];
+    arr[0] = temp;
   }
 
   return arr;
 }
 
-fdescribe('rotate', () => {
-  xit('test #1', () => {
+describe('rotateClockWise', () => {
+  it('test #1', () => {
     expect(rotateClockWise([1, 2, 3, 4, 5], 5)).toEqual([1, 2, 3, 4, 5]);
   });
-  xit('test #2', () => {
+  it('test #2', () => {
     expect(rotateClockWise([1, 2], 2)).toEqual([1, 2]);
   });
   it('test #3', () => {
     expect(rotateClockWise([1, 2, 3, 4, 5], 2)).toEqual([4, 5, 1, 2, 3]);
+  });
+  it('test #4', () => {
+    expect(rotateClockWise([1, 2], 1)).toEqual([2, 1]);
+  });
+  it('test #5', () => {
+    expect(rotateClockWise([1, 2, 3, 4, 5, 6, 7], 3)).toEqual([
+      5, 6, 7, 1, 2, 3, 4,
+    ]);
   });
 });
 /**
