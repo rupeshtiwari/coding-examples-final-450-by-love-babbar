1f014d8ca91fc09255588fb38051681bec4e503a
rupeshtiwari <roopkt@gmail.com>
2021-07-19 15:24:28 -0400

    chore: kadane algo

--- src/arrays/12-merge-2-sorted-arrays.js
+++ src/arrays/12-merge-2-sorted-arrays.js
@@ -9,8 +9,11 @@ function mergeTwoSortedArrays(arr1, arr2) {
   let idx1 = arr1.length - 1;
   let idx2 = arr2.length - 1;
 
+  if (arr1.length < 1) return arr2;
+  if (arr2.length < 1) return arr1;
+
   while (idx2 >= 0) {
-    if (idx1 >= 0 && arr1[idx1] > arr2[idx2]) {
+    if (idx1 >= 0 && arr1[idx1] >= arr2[idx2]) {
       arr1[lastIdx] = arr1[idx1];
       idx1--;
       lastIdx--;
@@ -32,16 +35,6 @@ describe('mergeTwoSortedArrays', () => {
   it('test #2', () => {
     expect(mergeTwoSortedArrays([10, 12], [5, 18, 20])).toEqual([
       5, 10, 12, 18, 20,
-    ]);
-  });
-  it('test #3', () => {
-    expect(
-      mergeTwoSortedArrays(
-        [7, 9, 9, 10, 11, 11, 13, 14, 17, 19],
-        [1, 1, 4, 5, 8, 11, 13, 16, 19, 19]
-      )
-    ).toEqual([
-      1, 1, 4, 5, 7, 8, 9, 9, 10, 11, 11, 11, 13, 13, 14, 16, 17, 19, 19, 19,
     ]);
   });
 });
