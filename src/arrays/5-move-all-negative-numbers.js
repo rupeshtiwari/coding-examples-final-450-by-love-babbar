3861a0c6622777506b4a45c2a6879715c03735b9
rupeshtiwari <roopkt@gmail.com>
2021-07-21 11:10:14 -0400

    chore: rotate and move algorithms

--- src/arrays/5-move-all-negative-numbers.js
+++ src/arrays/5-move-all-negative-numbers.js
@@ -1,37 +1,40 @@
 // https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/
-function moveAllNegatives(array) {
-  partition(array, 0, array.length - 1);
+// 
+class Solution {
+  moveAllNegatives(array) {
+    this.partition(array, 0, array.length - 1);
 
-  return array;
-}
-
-function partition(array, startIdx, endIdx) {
-  const pivot = 0;
-  let partitionIdx = 0;
-
-  for (let idx = startIdx; idx < endIdx; idx++) {
-    if (array[idx] < pivot) {
-      swap(idx, partitionIdx, array);
-      partitionIdx++;
-    }
+    return array;
   }
 
-  swap(partitionIdx, endIdx, array);
-}
+  partition(array, startIdx, endIdx) {
+    const pivot = 0;
+    let partitionIdx = 0;
 
-function swap(idx1, idx2, array) {
-  const temp = array[idx2];
-  array[idx2] = array[idx1];
-  array[idx1] = temp;
+    for (let idx = startIdx; idx < endIdx; idx++) {
+      if (array[idx] < pivot) {
+        this.swap(idx, partitionIdx, array);
+        partitionIdx++;
+      }
+    }
+
+    this.swap(partitionIdx, endIdx, array);
+  }
+
+  swap(idx1, idx2, array) {
+    const temp = array[idx2];
+    array[idx2] = array[idx1];
+    array[idx1] = temp;
+  }
 }
 
 describe('moveAllNegatives', () => {
-  it('test #1', () => {
-    expect(moveAllNegatives([-12, 11, -13, -5, 6, -7, 5, -3, -6])).toEqual([
-      -12, -13, -5, -7, -3, -6, 5, 6, 11,
-    ]);
+  xit('test #1', () => {
+    expect(
+      new Solution().moveAllNegatives([-12, 11, -13, -5, 6, -7, 5, -3, -6])
+    ).toEqual([-12, -13, -5, -7, -3, -6, 5, 6, 11]);
   });
   it('test #2', () => {
-    expect(moveAllNegatives([0, -1, 0])).toEqual([-1, 0, 0]);
+    expect(new Solution().moveAllNegatives([0, -1, 0])).toEqual([-1, 0, 0]);
   });
 });
--- src/arrays/7-rotate-n-times.js
+++ src/arrays/7-rotate-n-times.js
@@ -1,54 +1,56 @@
 /**
  * https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
+ * https://www.youtube.com/watch?v=gmu0RA5_zxs
+ * O(k+n) time | O(1)
  */
-function rotateClockWise(arr, n) {
-  //code here
+class RotateClockWise {
+  execute(arr, k) {
+    //code here
+    let length = arr.length;
+    k = k % length;
 
-  const length = arr.length;
-  if (n % length === 0) return arr;
-  if (n > length) n = n % length;
-
-  if (length < 2) return arr;
-
-  if (length < 3) {
-    let temp = arr[0];
-    arr[0] = arr[1];
-    arr[1] = temp;
+    this.reverse(arr, 0, length - 1);
+    this.reverse(arr, 0, k - 1);
+    this.reverse(arr, k, length - 1);
 
     return arr;
   }
 
-  for (let i = 0; i < n; i++) {
-    let temp = arr[1];
-    arr[1] = arr[0];
-
-    for (let idx = 2; idx < length; idx++) {
-      let current = arr[idx];
-      arr[idx] = temp;
-      temp = current;
+  reverse(arr, startIdx, endIdx) {
+    while (startIdx <= endIdx) {
+      this.swap(arr, startIdx, endIdx);
+      startIdx++;
+      endIdx--;
     }
-
-    arr[0] = temp;
   }
 
-  return arr;
+  swap(arr, startIdx, endIdx) {
+    const temp = arr[startIdx];
+    arr[startIdx] = arr[endIdx];
+    arr[endIdx] = temp;
+  }
 }
 
 describe('rotateClockWise', () => {
+ 
+
   it('test #1', () => {
-    expect(rotateClockWise([1, 2, 3, 4, 5], 5)).toEqual([1, 2, 3, 4, 5]);
+    expect(new RotateClockWise().execute([1, 2, 3, 4, 5], 5)).toEqual([1, 2, 3, 4, 5]);
   });
+
   it('test #2', () => {
-    expect(rotateClockWise([1, 2], 2)).toEqual([1, 2]);
+    expect(new RotateClockWise().execute([1, 2], 2)).toEqual([1, 2]);
   });
+
   it('test #3', () => {
-    expect(rotateClockWise([1, 2, 3, 4, 5], 2)).toEqual([4, 5, 1, 2, 3]);
+    expect(new RotateClockWise().execute([1, 2, 3, 4, 5], 2)).toEqual([4, 5, 1, 2, 3]);
   });
   it('test #4', () => {
-    expect(rotateClockWise([1, 2], 1)).toEqual([2, 1]);
+    expect(new RotateClockWise().execute([1, 2], 1)).toEqual([2, 1]);
   });
+
   it('test #5', () => {
-    expect(rotateClockWise([1, 2, 3, 4, 5, 6, 7], 3)).toEqual([
+    expect(new RotateClockWise().execute([1, 2, 3, 4, 5, 6, 7], 3)).toEqual([
       5, 6, 7, 1, 2, 3, 4,
     ]);
   });
